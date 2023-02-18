3b49f932051b1ce3dcb1c03d8ad25cbf9eaf1fbf
OlgaZima <115362618+OlgaZima@users.noreply.github.com>
2023-02-18 17:38:25 +0300

    Add files via upload

--- bracket.py
+++ bracket.py
@@ -10,19 +10,34 @@ def check_brackets(brackets_row: str) -> bool:
     if not isinstance(brackets_row, str):
         raise TypeError()
 
-    stack_ = 0
-    for i in range(len(brackets_row)):
-        if stack_ == 0 and brackets_row[i] == ")":
+    # stack_ = 0
+    # for i in range(len(brackets_row)):
+    #     if stack_ == 0 and brackets_row[i] == ")":
+    #         return False
+    #         break
+    #     elif brackets_row[i] == "(":
+    #         stack_ += 1
+    #     elif brackets_row[i] == ")":
+    #         stack_ -= 1
+    # if stack_ == 0:
+    #     return True
+    # else:
+    #     return False
+
+    stack_ = []
+    for i in brackets_row:
+        if len(stack_) == 0 and i == ")":
             return False
             break
-        elif brackets_row[i] == "(":
-            stack_ += 1
-        elif brackets_row[i] == ")":
-            stack_ -= 1
-    if stack_ == 0:
-        return True
-    else:
+        else:
+            if i == "(":
+                stack_.append(i)
+            elif i == ")":
+                stack_.pop()
+    if stack_:
         return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
